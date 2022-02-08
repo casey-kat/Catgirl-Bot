ffcde85a21b16ecbaa3094f39e392f479c6175ca
casey <realparaxenos@gmail.com>
2022-02-08 11:48:01 -0500

    - removed flirt stuff (more tedious than it was funny) - improved homestuck joke

--- catgirl_bot.py
+++ catgirl_bot.py
@@ -1,8 +1,12 @@
 import discord
 from discord.ext import commands
+
+from config import TOKEN
+
 import re
 import time
-from config import TOKEN
+import random
+
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -15,8 +19,6 @@ infile = open('./name_filter.txt', 'r')
 names = infile.read().splitlines()
 infile.close()
 
-flirt_ban_limit = 200
-
 # ------------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -28,64 +30,27 @@ async def on_ready():
 @bot.event
 async def on_message(message):
 
-    # If message is from this bot, do nothing (bit reduntant due to the next section)
+    # If message is from this bot, do nothing (bit reduntant due to the next section, but ill keep anyway for safeguarding)
     if message.author == bot.user:
         return
-
-    # -------------------------------------------------------------------------------------
 
     # If message is from any bot at all, do nothing (for pluralkit users)
     if message.author.bot:
         return
 
-    # Will check a sent message, and if it could be a valid kid name (by following the letter count conventions) then it will reply saying so
-    # If the message can be a valid kid name, send a message saying so.
-    match = re.match(r'[a-zA-Z]{4} [a-zA-Z]{6,7}$', message.content)
-    if match and match[0].lower() not in names:
-        await message.channel.send(f'"{match[0]}" is a valid kid name.')
+    # 25% the time will check a sent message, and if it could be a valid kid name (by following the letter count conventions) then it will reply saying so
+    if random.random() < 0.25 or message.channel.id == 940644970438729768:
+        # If the message can be a valid kid name, send a message saying so.
+        match = re.search(
+            r'(?:\W|^)([a-zA-Z]{4}) ([a-zA-Z]{6,7})(?:\W|$)', message.content)
+        if match and match[0].lower() not in names:
+            await message.channel.send(f'"{match[0].strip()}" is a valid kid name')
 
-    # If the message can be a valid troll name, send a message saying so.
-    match = re.match(r'[a-zA-Z]{6} [a-zA-Z]{6}$', message.content)
-    if match and match[0].lower() not in names:
-        await message.channel.send(f'"{match[0]}" is a valid troll name.')
-
-    # -------------------------------------------------------------------------------------
-
-    # If message is outside the mod-commands channel, return
-    if message.channel.id != mod_commands_channel_id:
-        return
-
-    # -------------------------------------------------------------------------------------
-
-
-# When a new member joins, send the result of flirt_ban_warning to the mod_commands channel
-@bot.event
-async def on_member_join(_):
-    channel = bot.get_channel(mod_commands_channel_id)
-    await channel.send(flirt_ban_warning(channel))
-
-
-# When a member leaves, send the result of flirt_ban_warning to the mod_commands channel
-@bot.event
-async def on_member_leave(_):
-    channel = bot.get_channel(mod_commands_channel_id)
-    await channel.send(flirt_ban_warning(channel))
-
-
-# When the command is inputted by a mod, send the result of flirt_ban_warning
-@bot.slash_command()
-@commands.has_role("mod")
-async def members_till_flirting_ban(channel):
-    await channel.respond(flirt_ban_warning(channel))
-
-
-# ------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-# Prints the time, and also the amount of members that must join for the count to reach flirt_ban_limit
-async def flirt_ban_warning(channel) -> str:
-    members_till_ban = flirt_ban_limit - channel.guild.member_count
-    return f"It is currently <t:{int(time.time())}> in your timezone. We are {members_till_ban} members away from the ban on flirting."
+        # If the message can be a valid troll name, send a message saying so.
+        match = re.search(
+            r'(?:\W|^)([a-zA-Z]{6}) ([a-zA-Z]{6})(?:\W|$)', message.content)
+        if match and match[0].lower() not in names:
+            await message.channel.send(f'"{match[0].strip()}" is a valid troll name')
 
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------
